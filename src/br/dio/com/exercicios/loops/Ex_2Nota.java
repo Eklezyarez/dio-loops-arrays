26624e069c1b8d84c8c2d5965b318457be151ee8
Eklezyarez <eklezyarez@gmail.com>
2022-07-30 12:18:57 -0300

    second exercise ok

---
+++ src/br/dio/com/exercicios/loops/Ex_2Nota.java
@@ -0,0 +1,27 @@
+package br.dio.com.exercicios.loops;
+/*
+Faça um programa que peça uma nota, entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido
+e continue pedindo
+até que o usuário informe um valor válido.
+*/
+
+import java.util.Scanner;
+
+public class Ex_2Nota {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Digite a nota: ");
+        nota = scan.nextInt();
+
+        while(nota < 0 || nota > 10){
+            System.out.println("Nota inválida!");
+            System.out.println("Digite novamente: ");
+            nota = scan.nextInt();
+        }
+    }
+
+}
