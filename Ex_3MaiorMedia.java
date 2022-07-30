75ff0283d6024ad73e2d52f8a0d4c572cbb7a8c6
Ivan Carlos de Oliveira Santos <104778982+Eklezyarez@users.noreply.github.com>
2022-07-30 12:55:20 -0300

    Add files via upload

---
+++ Ex_3MaiorMedia.java
@@ -0,0 +1,33 @@
+package br.dio.com.exercicios.loops;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que leia 5 números
+e informe o maior número
+e a média desses números.
+*/
+public class Ex_3MaiorMedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+        int count = 0;
+        int maior = 0;
+        int soma = 0;
+
+        do{
+            System.out.println("Informe o número: ");
+            num = scan.nextInt();
+            soma = soma + num;
+
+            if(num > maior){
+                maior = num;
+            }
+            count++;
+        }while(count < 5);
+
+        System.out.println("O maior número é: " + maior);
+        System.out.println("A média é: " + (soma/count));
+    }
+}
